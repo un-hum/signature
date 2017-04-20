3b630873b07ea61560d2d9822939c9d7be23b02a
czc <383418809@qq.com>
2017-04-20 15:48:35 +0800

    v0.1 修改结构

--- signature.js
+++ signature.js
@@ -1,3 +1,4 @@
+"use strict";
 var signature = new _signature()
 
 function _signature(){
@@ -16,24 +17,24 @@ _signature.prototype.fnMove = function(event){
 	this.pen.stroke();	
 }
 
-_signature.prototype.fnDown = function(event){
-	var e = event || window.event;	
+_signature.prototype.fnDown = function(event){	
+	var e = event || window.event; 	
 	var x = e.clientX - this.canvas.offsetLeft;
 	var y = e.clientY - this.canvas.offsetTop;
 	//初始化"画笔"
-	this.pen = this.canvas.getContext('2d')
+	this.pen = this.canvas.getContext('2d')	
 	//绘制开始
 	this.pen.beginPath();
-	this.pen.strokeStyle = this.canvas.color;
-    this.pen.fillStyle = this.canvas.color;
-    this.pen.lineWidth = this.canvas.lineWidth;
+	this.pen.strokeStyle = this.color;
+    this.pen.fillStyle = this.bg;           
+    this.pen.lineWidth = this.lineWidth;
 	this.pen.lineTo(x,y);
 
 	var _this = this
 
 	//创建用户滑动轨迹
 	document.onmousemove = function(event){
-		_this.fnMove()
+		_this.fnMove(event)
 	}	
 
 	//终止签名
@@ -57,18 +58,31 @@ _signature.prototype.createCanvas = function(ele){
 	ele.canvas = c
 }
 
+_signature.prototype.config = function(params){
+	var _this = this
+	params.dom ? function(){
+		alert('禁止修改dom')
+		return
+	}() : function(){
+		for(var p in params){			
+			_this.tmp[p] = _this[p]							
+			_this[p] = params[p]
+		}
+	}()
+}
+
 _signature.prototype.init = function(params){
 	if(params.dom){
 		this.dom = params.dom;
 		this.width = params.width ? params.width : document.querySelector(this.dom).offsetWidth;
 		this.height = params.height ? params.height : document.querySelector(this.dom).offsetHeight;
 		this.theme = params.theme ? params.theme : 'WB';//WB：经典黑线白底 BW：白线黑底
-		this.lineWidth = params.lineWidth;		
+		this.lineWidth = params.lineWidth ? params.lineWidth : 1;		
+		this.tmp = {}	
 		switch(this.theme){
 			case 'WB':				
 				this.color = 'black'
 				this.bg = 'white'
-				console.log(this.bg)
 			break;
 			case 'BW':
 				this.color = 'white'
@@ -87,8 +101,8 @@ _signature.prototype.init = function(params){
 
 		var _this = this
 
-		this.canvas.onmousedown = function(){
-			_this.fnDown()
+		this.canvas.onmousedown = function(event){			
+			_this.fnDown(event)
 		}
 	}else{
 		alert('please add Document')
